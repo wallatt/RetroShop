7bb1d2c183ef61bca9146c990e0ea420c8d2bd68
walter lacoste <wlacoste@outlook.com>
2022-09-01 14:08:42 -0300

    so far so good

---
+++ src/main/java/com/example/RetrosShop/RetrosShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.RetrosShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetrosShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetrosShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/RetrosShop/RetrosShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RetrosShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetrosShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
